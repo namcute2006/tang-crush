bbd358b5e6b607c6bde90ac07bf15cb322651af3
aleksuyb <85509917+aleksuyb@users.noreply.github.com>
2021-06-08 18:32:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Thư yêu dấu à!',
+    introTitle: 'X yêu dấu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
