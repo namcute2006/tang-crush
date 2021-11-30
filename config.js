8dfcc12a05cd5adf65a677f215bb0f7f3c97adec
namcute2006 <95282867+namcute2006@users.noreply.github.com>
2021-11-30 18:10:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,5 +17,5 @@ const CONFIG = {
     mess: 'Em biết mà 🥰. Yêu em nhiều lắm :3 ',
     messDesc: 'Tối nay 7h mình gặp nhau nha ❤️.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/waifu.anime //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
