32a092b2d7cc859e718350870a9f51cd2195bb5b
namcute2006 <95282867+namcute2006@users.noreply.github.com>
2021-11-30 18:16:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu em nhiều lắm :3 ',
-    messDesc: 'Tối nay 7h mình gặp nhau nha ❤️.',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều lắm :3 ',
+    messDesc: 'Tối nay 7h ib cho em nhá >< ❤️.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
